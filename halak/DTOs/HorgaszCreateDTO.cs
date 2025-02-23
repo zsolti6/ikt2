984c252e96aafea660790a07f4c274f12ed69b95
Gáspár Zsolt <144207546+zsolti6@users.noreply.github.com>
2025-02-23 13:20:56 +0100

    Delete halak directory

--- halak/DTOs/HorgaszCreateDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace halak.DTOs
-{
-    public class HorgaszCreateDTO
-    {
-        public string Nev { get; set; } = null!;
-        public int Eletkor { get; set; }
-        public List<FogasDTO> Fogasok { get; set; }
-    }
-}
